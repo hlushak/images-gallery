313744ae1147b038bdb6445584c9abfd9a723bf0
Maryna Hlushak <hlushak.m.v@gmail.com>
2021-03-15 19:17:54 +0200

    initial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
-const App = () => {
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+const App = (props) => {
   return (
     <div className="App">
-      <h1>Images Gallery</h1>
+      <Header title ="Images Gallery"/>
     </div>
   );
 }
